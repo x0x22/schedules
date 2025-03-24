1b01c9147b9426689ea3cc550e265b53a02b7316
isom <smallsol.fa@gmail.com>
2025-03-24 11:56:21 +0900

    시작

---
+++ src/test/java/com/sparta/schedules/SchedulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.schedules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
